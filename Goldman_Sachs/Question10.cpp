529bebb5a9aceffb098778d6f988ba6e66491cbc
Raksha Fauzdar <71915465+RakshaFauzdar@users.noreply.github.com>
2022-01-04 09:43:23 +0530

    Add files via upload

---
+++ Goldman_Sachs/Question10.cpp
@@ -0,0 +1,44 @@
+// DECODE_STRING
+class Solution{
+public:
+string d(string &s,int &i)
+{
+    string ans;
+       
+        int n=s.length();
+        while(i<n && s[i]!=']')
+        {
+            if(isdigit(s[i]))
+            {
+                int k=0;
+                while(i<n && isdigit(s[i]))
+                {
+                    k=k*10+s[i++]-'0';
+                }
+                i++;
+                string s1=d(s,i);
+                while(k--)
+                {
+                    ans+=s1;
+                    
+                }
+                i++;
+                
+            }
+            else
+            {
+                ans+=s[i++];
+            }
+        }
+        return ans;
+    
+}
+    string decodedString(string s){
+       int i=0;
+       return d(s,i);
+        // code here
+    }
+};
+
+// TIME COMPLEXITY :O(N)
+// SPACE COMPLEXITY :O(N)
