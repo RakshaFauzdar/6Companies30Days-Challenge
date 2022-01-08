e1bda6a1e7c7caa11c1d312362699e268001aaf2
Raksha Fauzdar <71915465+RakshaFauzdar@users.noreply.github.com>
2022-01-08 18:37:25 +0530

    Add files via upload

---
+++ Amazon/Question5.cpp
@@ -0,0 +1,13 @@
+// COUNT_WAYS_TO_NTH_STAIRS
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+       return 1+(m/2);
+        
+        // your code here
+    }
+};
